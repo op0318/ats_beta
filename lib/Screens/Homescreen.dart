f00e743a230d242420aca85c48c0dbe7cbc160aa
DEVI2899 <amirthadevicse@gmail.com>
2023-03-15 17:07:08 +0530

    Home Screen

--- lib/Screens/Homescreen.dart
+++ lib/Screens/Homescreen.dart
@@ -8,8 +8,208 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Controller = PageController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double Height = MediaQuery.of(context).size.height;
+    double Width = MediaQuery.of(context).size.width;
+    return  Scaffold(
+      backgroundColor:Color(0xff3583cc),
+      body: PageView(
+        controller: Controller,
+        children: [
+          Page1(),
+          Page2(),
+          Page3(),
+        ],
+      ),
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   children: [
+      //     // page view
+      //     SizedBox(
+      //       height: Height/1.7,
+      //       child: PageView(
+      //         controller: Controller,
+      //         children: const [
+      //           Page1(),
+      //           Page2(),
+      //           Page3(),
+      //         ],
+      //       ),
+      //     ),
+      //     SizedBox(height: Height/25,),
+      //     // dot indicators
+      //     SmoothPageIndicator(
+      //       controller: Controller,
+      //       count: 3,
+      //       effect: ScrollingDotsEffect(
+      //         //activeDotColor: const Color(0xff1d2a40),
+      //         dotColor: Colors.deepPurple.shade100,
+      //         dotHeight: Height/60,
+      //         dotWidth: Width/40,
+      //         spacing: Width/20.5,
+      //         //verticalOffset: 50,
+      //
+      //       ),
+      //     ),
+      //     Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //       children: [
+      //       Padding(
+      //         padding: EdgeInsets.only(right:Width/5),
+      //         child: TextButton(onPressed: (){},
+      //             child: Text('Skip',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),)),
+      //       ),
+      //        Padding(
+      //         padding: EdgeInsets.only(left: Width/5),
+      //         child: TextButton(onPressed: (){}, child:  Text('Next',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),),
+      //         ),
+      //       )
+      //     ],)
+      //
+      //   ],
+      // ),
+    );
+  }
+}
+
+class Page1 extends StatefulWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  State<Page1> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  @override
+  Widget build(BuildContext context) {
+    double Height = MediaQuery.of(context).size.height;
+    double Width = MediaQuery.of(context).size.width;
+    return  Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+          const Image(image: AssetImage('assets/images/page1.png')),
+          SizedBox(height: Height/10,),
+          Text('Tracking' , style: TextStyle(fontSize:Height/30, color: const Color(0xffffffff), fontWeight: FontWeight.bold ),),
+          SizedBox(height: Height/60,),
+          Padding(
+            padding:  EdgeInsets.only(left: Width/15, right: Width/20),
+            child: Text('App is based on tracking the location details of the employee login & logout time',
+                style:TextStyle(color: const Color(0xffF5FAFD), fontSize: Height/30, fontWeight:  FontWeight.normal) ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right:Width/4,top: Height/9),
+                child: TextButton(onPressed: (){},
+                    child: Text('Skip',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: Width/5, top: Height/9),
+                child: TextButton(onPressed: (){}, child:  Text('Next',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),),
+                ),
+              )
+            ],)
+        ],
+      ),
+    );
+  }
+}
+
+class Page2 extends StatefulWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  @override
+  Widget build(BuildContext context) {
+    double Height = MediaQuery.of(context).size.height;
+    double Width = MediaQuery.of(context).size.width;
+    return  Container(
+      child: Column(
+        children:  [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical:Height/10, horizontal: Width/20  ),
+            child: const Image(image: AssetImage('assets/images/page2.png')),
+          ),
+          Text('Monitoring' , style: TextStyle(fontSize:Height/30, color: const Color(0xffffffff), fontWeight: FontWeight.bold ),),
+          SizedBox(height: Height/50,),
+          Padding(
+            padding:  EdgeInsets.only(left: Width/15, right: Width/20),
+            child: Text('App is Based on Monitoring Daily and Monthly Attendance Details of employees',
+              style:TextStyle(color:  const Color(0xffF5FAFD), fontSize: Height/30, fontWeight:  FontWeight.normal),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right:Width/5 ,top: Height/9),
+                child: TextButton(onPressed: (){},
+                    child: Text('Skip',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: Width/5, top: Height/9),
+                child: TextButton(onPressed: (){}, child:  Text('Next',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),),
+                ),
+              )
+            ],)
+
+        ],
+      ),
+    );
+  }
+}
+
+class Page3 extends StatefulWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  State<Page3> createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+  @override
+  Widget build(BuildContext context) {
+    double Height = MediaQuery.of(context).size.height;
+    double Width = MediaQuery.of(context).size.width;
+    return  Container(
+      child: Column(
+        mainAxisAlignment:  MainAxisAlignment.center,
+        children:  [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Width/10, vertical: Height/10),
+            child: const Image(image: AssetImage('assets/images/page3.png')),
+          ),
+          SizedBox(height: Height/40,),
+          Text('Visualizing' , style: TextStyle(fontSize:Height/30, color: const Color(0xffffffff), fontWeight: FontWeight.bold ),),
+          SizedBox(height: Height/50,),
+          Padding(
+            padding: EdgeInsets.only(left: Width/15, right: Width/20),
+            child: Text('App  is based on Visualizing the employee details', style:TextStyle(color:  Color(0xffF5FAFD), fontSize: Height/30, fontWeight:  FontWeight.normal),),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right:Width/5, top: Height/8),
+                child: TextButton(onPressed: (){},
+                    child: Text('Skip',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: Width/5, top: Height/8),
+                child: TextButton(onPressed: (){}, child:  Text('Next',style: TextStyle(fontSize: Height/30, color: Color(0xffe6e6e6)),),
+                ),
+              )
+            ],)
+        ],
+      ),
+    );
   }
 }
