88e42efe3a26e28fbffe5873a62fa4475f6577d9
op0318 <iamthiru9818@gmail.com>
2023-03-15 16:10:24 +0530

    Adding the db Authntication code to login page and sdk files version change

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 
 
+import 'package:ats_beta/Screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const statemanagement());
+  runApp(const MyHome());
 }
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: statemanagement(),
+    );
+  }
+}
+
+
+
 
 class statemanagement extends StatefulWidget {
   const statemanagement({Key? key}) : super(key: key);
@@ -14,81 +29,27 @@ class statemanagement extends StatefulWidget {
 }
 
 class _statemanagementState extends State<statemanagement> {
-  final empcontroller=TextEditingController();
-  List database=[
-    {'empid':['10801', '10802', '10803', '10804', '10805', '10806', '10807', '10809', '10901']},
-    {'name':['thiru','devi','vini','suba','Aravi','venkat','aji','aji','sadhu','reha']}];
-
-
-   String? empno;
-  void checkde() {
-    for (int i = 0; i < (database[0]['empid'] as List<String>).length; i++) {
-      if ((database[0]['empid'] as List<String>)[i] == empcontroller.text.toString()) {
-        print("welcome to Sightspectrum");
-        return;
-      }
-      else {
-      }
-
-    }
-    print('please enter the valid mail id');
-  }
-
-
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: Container(
-            height: double.infinity,
+    return Scaffold(
+          body:Container( height: double.infinity,
             width: double.infinity,
-            color: Colors.blue,
-            child: Column(
-              children: [
-                Text("Login Page", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
-              ),const SizedBox(height: 20,),
-                Row(children: [Text(style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                    "Enter the valid id:"),SizedBox(width: 10,),
-                  Container(
-                    color: Colors.white,
-                    height: 50,
-                  width: 200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: TextField(
-                      controller: empcontroller,
-                    decoration: InputDecoration(focusColor: Colors.white,
-                        border: OutlineInputBorder(
-                           )),
-                    ),
-                  ),
-                )],),
-                SizedBox(height: 20,),
-                Container(height: 40,
-                  width: 200,
-                  child: ElevatedButton(style: ButtonStyle(
-
-                  ),
-                      onPressed: checkde, child: Text(style: TextStyle(color: Colors.white),'Sign INNNNNNNN')),
-                )
-
-
-
-              ],
+            child: Center(
+              child: Column(
+                children: [
+                  Text('ATS',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold)),
+                  SizedBox(height: 50,),
+                  ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.yellow)),
+                      onPressed: (){
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Employeepage(),));
+                      }, child: Text('WElcome'))
+                ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
-  }
+          )
+
+    ) ;}
 }
 
 
