ad88686e7e6484ffb7b1fabe3f7875cdc416fed7
op0318 <iamthiru9818@gmail.com>
2023-03-20 12:36:42 +0530

    Adding the sssssssssss

--- lib/Screens/Vaildcheck.dart
+++ lib/Screens/Vaildcheck.dart
@@ -18,7 +18,7 @@ class valid extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.only(right: Width/10, left: Width/10),
-            child:  Text('your EmployeeID must be atleast 5 numbers long', style: TextStyle(color: const Color(0xffA19F9F),fontSize:Height/35, fontWeight:FontWeight.normal),),
+            child:  Text('your EmployeeID must be atleast 5 numbers longsssss', style: TextStyle(color: const Color(0xffA19F9F),fontSize:Height/35, fontWeight:FontWeight.normal),),
           ),
           SizedBox(height :Height/80),
           RaisedButton(onPressed: (){},color: const Color(0xfff14141),
